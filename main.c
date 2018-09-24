b2a9d16bc13230aae870de4f57e7b24e61d1ab55
student <ialeos@miners.utep.edu>
2018-09-24 11:42:06 -0600

    Added string_length function

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
 
 int main(){
 
+  int i = 0;//index holder
   int x;//output holder
   int cont = 1;//boolean
 
-  char* sentence;
+  char *sentence;
+  printf(">");
+}
+
+int string_length(char *str) {
+  int count = 0; //length holder
+  while (*str != '\0') {
+    count++; //count each letter
+    *str++; //traverse char array
+  }
+  return count;
 }
