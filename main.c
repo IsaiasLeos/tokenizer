e1016bccc17eb63fbf043c84710afeffe37868c3
student <ialeos@miners.utep.edu>
2018-09-24 12:37:13 -0600

    added get_user_input() function + implemented string_length()

--- main.c
+++ main.c
@@ -2,14 +2,34 @@
 #include "tokenizer.h"
 #include "history.h"
 
+void get_user_input(char*);
+
 int main(){
 
-  int i = 0;//index holder
   int x;//output holder
   int cont = 1;//boolean
 
   char *sentence;
+  while(1){
+    get_user_input(sentence);
+    x = string_length(&sentence[0]);
+    printf("length -> %d",x);
+    printf("\n");
+  }
+}
+
+void get_user_input(char* sentence){
   printf(">");
+  int i = 0;//index holder
+  char c = getchar();
+
+  while(c != '\n'){
+    sentence[i++] = c;
+    c = getchar();
+  }
+  sentence[i] = '\0';//make sure array ends
+  printf("%s",sentence);
+  printf("\n");
 }
 
 int string_length(char *str) {
@@ -21,6 +41,6 @@ int string_length(char *str) {
   return count;
 }
 
-int is_valid_character(char c){
+char is_valid_character(char c){
   return c > 31 && c < 127;
 }
