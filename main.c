3dfed1041c389c431c47f4dd2f0ec92f472a16af
student <ialeos@miners.utep.edu>
2018-09-25 16:46:53 -0600

    Added method to fix user sentence + added function count_words + is_valid_character()

--- main.c
+++ main.c
@@ -2,43 +2,66 @@
 #include "tokenizer.h"
 #include "history.h"
 
-/*Obtains user input and populates the given char array. */
 void get_user_input(char*);
+/* Would read user input and remove any extra spaces. */
+void fix_user_sentence(char*);
 
-int main(){
 
-  int x;//output holder
-  int cont = 1;//boolean
 
-  char *sentence;
-  while(1){
+int main() {
+  int x; //output holder
+  int cont = 1;
+  char *sentence; //to carry all user input
+
+
+
+  while (1 && cont) {
     get_user_input(sentence);
-    x = string_length(&sentence[0]);
-    printf("length -> %d",x);
+    printf("finding length...");
     printf("\n");
-
-    printf("not implemented - is_valid_character()");
-
-    printf("not implemented - find_word_start()");
-
-    printf("not implemented - find_word_end()");
-
-    printf("not implemented - cound_words()");
+    x = string_length(&sentence[0]); //obtain length of string
+    printf("length - %d", x);
+    printf("\n");
+    printf("not implemented");
+    printf("\n");
+    printf("not implemented");
+    printf("\n");
+    x = find_word_start(&sentence[0], 0); //obtain index to next word
+    printf("next word start at - %d", x);
+    printf("\n");
+    x = find_word_end(&sentence[0], 0); //obtain index to next word
+    printf("next word end at - %d", x);
+    printf("\n");
+    printf("not implemented");
+    printf("\n");
   }
+  return 0;
 }
 
-void get_user_input(char* sentence){
-  printf(">");
-  int i = 0;//index holder
-  char c = getchar();
 
-  while(c != '\n'){//populate char array
+void get_user_input(char* sentence) {
+  printf(">");
+  int i = 0; //index holder
+  char c = getchar();
+  while (c != '\n') {
     sentence[i++] = c;
     c = getchar();
   }
-  sentence[i] = '\0';//make sure array ends
-  printf("%s",sentence);//print sentence
+  sentence[i] = '\0'; //make sure string is ended
+  fix_user_sentence(sentence);
+  printf("%s", sentence); //print sentence
   printf("\n");
+
+}
+
+void fix_user_sentence(char* str) {
+  int i, j;
+  for (i = j = 0; str[i]; ++i) {
+    if (str[i] != ' ' || (str[i - 1]) != ' ') {
+      str[j++] = str[i];
+    }
+  }
+  str[j] = '\0';
 }
 
 int string_length(char *str) {
@@ -50,19 +73,51 @@ int string_length(char *str) {
   return count;
 }
 
-char is_valid_character(char c){
-  return c > 31 && c < 127;
-}
-
-int find_word_start(char* str, int i){
-  int index = 0;//index of the next word
-  char c = 1;
-  while(str[i] != '\0'){
-    if(str[i] == ' ' && c){
-      index = i + 1;//save position of next word
-      c = 0;//found word, prevent re-run
+int find_word_start(char* str, int i) {
+  int index = 0; //index of next word
+  char c = 1; //boolean
+  while (str[i] != '\0') {
+    if (str[i] == ' ' && c) {//TODO improve
+      index = i + 1; //set index of next word
+      c = 0;
     }
     i++;
   }
   return index;
+}
+
+
+
+int find_word_end(char* str, int i) {
+  int index = 0; //index of next word
+  char c = 1, d = 1; //boolean
+  while (str[i] != '\0' || str[i] == ' ') {
+    i++;
+  }
+  while (str[i] != '\0' || str[i] == ' ') {
+    i++;
+  }
+  return index;
+}
+
+
+
+char is_valid_character(char c) {
+  return c > 31 && c < 127;
+}
+
+
+
+int count_words(char* str) {
+  int i = 0;
+  int words = 0;
+  char c;
+  while (str[i] != '\0') {
+    c = (str[i] == ' ');
+    if (c) {
+      words++;//count for whitespace
+    }
+    i++;
+  }
+  return words + 1;
 }
