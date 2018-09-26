c23cbc7c6938fe63c2fe1d746bd0bb1d3a5b4f5b
student <ialeos@miners.utep.edu>
2018-09-26 12:10:11 -0600

    implemented find_word_start + find_word_end + count_words

--- main.c
+++ main.c
@@ -10,26 +10,12 @@ int main() {
   int cont = 1;
   char *sentence; //to carry all user input
 
-  while (1 && cont) {
+  while (1) {
     get_user_input(sentence);
-    printf("finding length...");
-    printf("\n");
     x = string_length(&sentence[0]); //obtain length of string
     printf("length - %d", x);
-    printf("\n");
-    printf("not implemented");
-    printf("\n");
-    printf("not implemented");
-    printf("\n");
-    x = find_word_start(&sentence[0], 0); //obtain index to next word
-    printf("next word start at - %d", x);
-    printf("\n");
-    x = find_word_end(&sentence[0], 0); //obtain index to next word
-    printf("next word end at - %d", x);
-    printf("\n");
-    x = count_words(sentence);
-    printf("counted words - %d",x);
-    printf("\n");
+    x = count_words(&sentence[0]);
+    printf("count - %d",x);
   }
   return 0;
 }
@@ -85,15 +71,12 @@ char is_valid_character(char c) {
 
 
 int count_words(char* str) {
-  int i = 0;
-  int words = 0;
-  char c;
-  while (str[i] != '\0') {
-    c = (str[i] == ' ');
-    if (c) {
-      words++;//count for whitespace
-    }
-    i++;
+  int start = 0, end = 0, counted = 0, i = 0;
+  while(str[i] != '\0'){
+    start = find_word_start(str,end);
+    end = find_word_end(str,start);
+    i = end;
+    counted++;
   }
-  return words + 1;
+  return counted;
 }
