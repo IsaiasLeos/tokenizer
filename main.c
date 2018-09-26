bcffaea84e66e99fbdc6742788cc2e085261ca6e
student <ialeos@miners.utep.edu>
2018-09-26 17:17:37 -0600

    Fixed bugs in find_word_end + fixed a few bugs + implemented count_words

--- main.c
+++ main.c
@@ -7,46 +7,52 @@ void get_user_input(char*);
 /* Would read user input and remove any extra spaces. */
 int main() {
   int x; //output holder
-  int cont = 1;
-  char *sentence; //to carry all user input
-
+  char size = 10;
+  char *sentence = (char*) malloc(size * sizeof (char));
+  free(sentence);
   while (1) {
     get_user_input(sentence);
-    x = string_length(&sentence[0]); //obtain length of string
-    printf("length - %d", x);
-    x = count_words(&sentence[0]);
-    printf("count - %d",x);
+    x = string_length(sentence);
+    printf("Length: %d", x);
+    printf("\n");
+    x = count_words(sentence);
+    printf("Words: %d", x);
+    printf("\n");
   }
   return 0;
 }
 
-
 void get_user_input(char* sentence) {
   printf(">");
-  int i = 0; //index holder
+  int i = 0;
   char c = getchar();
   while (c != '\n') {
     sentence[i++] = c;
     c = getchar();
   }
-  sentence[i] = '\0'; //make sure string is ended
-  //printf("%s", sentence); //print sentence
-  printf("\n");
+  sentence[i] = '\0';
+}
 
+void copy_array(char* source, char* destination, int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    destination[i] = source[i];
+  }
+  printf("%s", destination);
 }
 
 int string_length(char *str) {
   int count = 0; //length holder
   while (*str != '\0') {
     count++; //count each letter
-    *str++; //traverse char array
+    str++; //traverse char array
   }
   return count;
 }
 
 int find_word_start(char* str, int i) {
   while (str[i] != '\0') {
-    if (is_valid_character(str[i])) {//TODO improve
+    if (is_valid_character(str[i])) {
       return i; //set index of next word
     }
     i++;
@@ -54,10 +60,15 @@ int find_word_start(char* str, int i) {
   return 0;
 }
 
+
+
 int find_word_end(char* str, int i) {
-  while(str[i] != '\0'){
-    if(!is_valid_character(str[i])) {
+  while (str[i] != '\0') {
+    if (!is_valid_character(str[i])) {
       return i;
+    }
+    if (str[i + 1] == '\0') {
+      return i++;
     }
     i++;
   }
