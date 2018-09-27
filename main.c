de5e0e1d98aabe53d4c36faa0746e21ee6610150
student <ialeos@miners.utep.edu>
2018-09-27 12:11:05 -0600

    Completed Milestone 2

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
-
-/*Obtains user input and populates the given char array. */
+/* Obtains user input and populates the given char array. */
 void get_user_input(char*);
-/* Would read user input and remove any extra spaces. */
+/* Copies the array given to the next. */
+void copy_array(char*, char*, int size);
+
 int main() {
   int x; //output holder
   char size = 10;
@@ -23,22 +25,24 @@ int main() {
 }
 
 void get_user_input(char* sentence) {
-  printf(">");//validation for input
+  printf(">");
   int i = 0;
-  char c = getchar();//get user input
-  while (c != '\n') {//populate array with user input
+  char c = getchar();
+  while (c != '\n') {
     sentence[i++] = c;
     c = getchar();
   }
   sentence[i] = '\0';
 }
 
+
+
 void copy_array(char* source, char* destination, int size) {
   int i;
-  for (i = 0; i < size; i++) {//copy from source to destination
+  for (i = 0; i < size; i++) {
     destination[i] = source[i];
   }
-  printf("%s", destination);//print for confirmation
+  printf("%s", destination);
 }
 
 int string_length(char *str) {
@@ -52,25 +56,21 @@ int string_length(char *str) {
 
 int find_word_start(char* str, int i) {
   while (str[i] != '\0') {
-    //check for a valid character
     if (is_valid_character(str[i])) {
-      return i; //return index of valid character
+      return i; //set index of next word
     }
-    i++;//incrementation for traversal
+    i++;
   }
   return 0;
 }
 
-
-
 int find_word_end(char* str, int i) {
   while (str[i] != '\0') {
-    //check for a non-valid character
     if (!is_valid_character(str[i])) {
-      return i;//return index of a invalid character
+      return i;
     }
     if (str[i + 1] == '\0') {
-      return i++;//no spacing found at the end of the word
+      return i++;
     }
     i++;
   }
@@ -78,16 +78,14 @@ int find_word_end(char* str, int i) {
 
 
 char is_valid_character(char c) {
-  return c > 32 && c < 127;//check for validation of character
+  return c > 32 && c < 127;
 }
-
-
 
 int count_words(char* str) {
   int start = 0, end = 0, counted = 0, i = 0;
-  while(str[i + 1] != '\0'){
-    start = find_word_start(str,end);//save starting index
-    end = find_word_end(str,start);//save ending index
+  while (str[i + 1] != '\0') {
+    start = find_word_start(str, end);//keep track of where a word starts
+    end = find_word_end(str, start);//keep track of where a word ends
     i = end;//save ending as current index
     counted++;//count valid words
   }
