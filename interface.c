d3f843c036e5b4bea5f8a05c730ea5163e3b4964
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-09-30 19:19:12 -0600

    fixed infinite loop

--- interface.c
+++ interface.c
@@ -4,28 +4,32 @@
 #include "history.h"
 /* Obtains user input and populates the given char array. */
 void get_user_input(char*);
+
 int main() {
-  int x; //output holder
-  while (1) {
-    char sentence[20];
-    get_user_input(sentence); //assign user input
-    x = string_length(sentence); //return length of user input
-    printf("Length: %d", x);
-    printf("\n");
-    x = count_words(sentence); //count words in user input
-    printf("Count: %d", x); //word count
-    printf("\n");
-    //free(sentence);
-  }
-  return 0;
+    int x; //output holder
+    char size = 10;
+
+    while (1) {
+        char *sentence = (char*) malloc(size * sizeof (char));
+        free(sentence);
+        get_user_input(sentence); //assign user input
+        x = string_length(sentence); //return length of user input
+        printf("Length: %d", x);
+        printf("\n");
+        x = count_words(sentence); //count words in user input
+        printf("Count: %d", x); //word count
+        printf("\n");
+    }
+    return 0;
 }
 
 void get_user_input(char* sentence) {
-  printf(">"); //input validation
-  char c = getchar(); //get user input
-  while (c != '\n') {//populate array with user input
-    *sentence++ = c;
-    c = getchar();
-  }
-  *sentence = '\0';
+    printf(">"); //input validation
+    int i = 0;
+    char c = getchar(); //get user input
+    while (c != '\n') {//populate array with user input
+        sentence[i++] = c;
+        c = getchar();
+    }
+    sentence[i] = '\0';
 }
--- tokenizer.c
+++ tokenizer.c
@@ -4,48 +4,49 @@
 #include "history.h"
 
 int string_length(char *str) {
-  int count = 0; //length holder
-  while (str[count] != '\0') {
-    count++; //count each letter
-  }
-  return count;
+    int count = 0; //length holder
+    while (str[count] != '\0') {
+        count++; //count each letter
+    }
+    return count;
 }
 
 int find_word_start(char* str, int i) {
-  while (str[i] != '\0') {
-    if (is_valid_character(str[i])) {
-      return i; //return index of a valid character
+    while (str[i] != '\0') {
+        if (is_valid_character(str[i])) {
+            return i; //return index of a valid character
+        }
+        i++; //traversal
     }
-    i++; //traversal
-  }
-  return 0;
+    return 0;
 }
 
 int find_word_end(char* str, int i) {
-  while (str[i] != '\0') {
-    if (!is_valid_character(str[i])) {
-      return i; //return index of a invalid character
+    while (str[i] != '\0') {
+        if (!is_valid_character(str[i])) {
+            return i; //return index of a invalid character
+        }
+        if (str[i + 1] == '\0') {
+            return i++; //if NULL character is found
+        }
+        i++;
     }
-    if (str[i + 1] == '\0') {
-      return i++; //if NULL character is found
-    }
-    i++;
-  }
 }
 
 char is_valid_character(char c) {
-  return c > 32 && c < 127; //character validation
+    return c > 32 && c < 127; //character validation
 }
 
 int count_words(char* str) {
-  int start = 0, end = 0, counted = 0, i = 0;
-  while (str[i + 1] != '\0') {
-    start = find_word_start(str, end); //keep track of where a word starts
-    end = find_word_end(str, start); //keep track of where a word ends
-    printf("Word at: %d - %d", start, end);
-    printf("\n");
-    i = end; //save ending as current index
-    counted++; //count valid words
-  }
-  return counted;
+    int start = 0, end = 0, counted = 0, i = 0;
+    while (str[i + 1] != '\0') {
+        start = find_word_start(str, end); //keep track of where a word starts
+        end = find_word_end(str, start); //keep track of where a word ends
+        printf("Word at: %d - %d", start, end);
+        printf("\n");
+        i = end; //save ending as current index
+        counted++; //count valid words
+        if (start == 0 && end == 0) break;
+    }
+    return counted;
 }
