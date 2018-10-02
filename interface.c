791f4aa0a0f9328a6fa3019988a90c940af5e5f9
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-10-02 00:06:17 -0600

    documentation + renaming variables

--- interface.c
+++ interface.c
@@ -11,8 +11,8 @@ void copy_array(char*, char*, int);
 int main() {
     char size = ORIGINALSIZE;
     while (1) {
-        char *userInput = (char*) malloc(size * sizeof (char));
-        char *userInputTemp = (char*) malloc(size * sizeof (char));
+        char *userInput = (char*) malloc(size * sizeof (char));//save user input
+        char *userInputTemp = (char*) malloc(size * sizeof (char));//temp input
         userInput = get_user_input(userInput, userInputTemp);
         int x = string_length(userInput); //return length of user input
         printf("Length: %d", x);
@@ -28,23 +28,23 @@ int main() {
 
 char* get_user_input(char *userInput, char *userInputTemp) {
 
-    printf(">");
+    printf(">");//output validation
     int i, size;
     char c;
 
-    size = ORIGINALSIZE;
+    size = ORIGINALSIZE;//create initial size
     c = getchar();
 
     while (c != '\n') {
-        userInput[i] = c;
-        c = getchar();
-        i++;
+        userInput[i] = c;//populate
+        c = getchar();//obtain next char
+        i++;//increment
         if (i >= size) {
             size *= 2;
-            copy_array(userInput, userInputTemp, size);
-            userInput = (char*) malloc(size * sizeof (char));
-            copy_array(userInputTemp, userInput, size);
-            userInputTemp = (char*) malloc(size * sizeof (char));
+            copy_array(userInput, userInputTemp, size);//copy user to temp
+            userInput = (char*) malloc(size * sizeof (char));//double user
+            copy_array(userInputTemp, userInput, size);//copy temp to user
+            userInputTemp = (char*) malloc(size * sizeof (char));//double temp
         }
     }
     userInput[i] = '\0';
--- tokenizer.c
+++ tokenizer.c
@@ -5,33 +5,33 @@
 
 #define ORIGINALSIZE 10
 
-int string_length(char *str) {
-    int count = 0; //length holder
-    while (str[count] != '\0') {
-        count++; //count each letter
+int string_length(char *userInput) {
+    int length = 0; //length holder
+    while (userInput[length] != '\0') {
+        length++; //count each letter
     }
-    return count;
+    return length;
 }
 
-int find_word_start(char* str, int i) {
-    while (str[i] != '\0') {
-        if (is_valid_character(str[i])) {
-            return i; //return index of a valid character
+int find_word_start(char* userInput, int index) {
+    while (userInput[index] != '\0') {
+        if (is_valid_character(userInput[index])) {
+            return index; //return index of a valid character
         }
-        i++; //traversal
+        index++; //traversal
     }
     return 0;
 }
 
-int find_word_end(char* str, int i) {
-    while (str[i] != '\0') {
-        if (!is_valid_character(str[i])) {
-            return i; //return index of a invalid character
+int find_word_end(char* userInput, int index) {
+    while (userInput[index] != '\0') {
+        if (!is_valid_character(userInput[index])) {
+            return index; //return index of a invalid character
         }
-        if (str[i + 1] == '\0') {
-            return i++; //if NULL character is found
+        if (userInput[index + 1] == '\0') {
+            return index++; //if NULL character is found
         }
-        i++;
+        index++;
     }
 }
 
@@ -39,11 +39,11 @@ char is_valid_character(char c) {
     return c > 32 && c < 127; //character validation
 }
 
-int count_words(char* str) {
+int count_words(char* userInput) {
     int start = 0, end = 0, counted = 0, i = 0;
-    while (str[i + 1] != '\0') {
-        start = find_word_start(str, end); //keep track of where a word starts
-        end = find_word_end(str, start); //keep track of where a word ends
+    while (userInput[i + 1] != '\0') {
+        start = find_word_start(userInput, end); //keep track of where a word starts
+        end = find_word_end(userInput, start); //keep track of where a word ends
         printf("Word at: %d - %d", start, end);
         printf("\n");
         i = end; //save ending as current index
