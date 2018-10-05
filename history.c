33b2c81d75d8777dc81caaf70486fbd2c6e8e645
Pantherinae <blade_master_pantherinae@hotmail.com>
2018-10-05 16:59:43 -0600

    fixed issue with ! commands

--- history.c
+++ history.c
@@ -4,67 +4,66 @@
 #include "history.h"
 
 List* init_history() {
-    List *history = (List*) malloc(sizeof (Item));
-    history->root = NULL;
-    return history;
+	List *history = (List*) malloc(sizeof (Item));
+	return history;
 }
 
 void add_history(List* list, char* userInput) {
-    
-    char *userInputTemp = (char*) malloc(string_length(userInput) * sizeof (char));
-    
-    int i = 0;
-    while (userInput[i]) {//copy string into a new string
-        userInputTemp[i] = userInput[i];
-        i++;
-    }
-    userInputTemp[i] = '\0';
-    
-    Item *node = list->root;
-    int index = 0;
-    
-    if (node == NULL) {//Insert item into the list for the first time
-        list->root = (Item*) malloc(sizeof (Item));
-        list->root->id = index;
-        list->root->str = userInputTemp;
-        list->root->next = 0;
-    } else {
-        index++;
-        //Traverse the list until iter->next is null
-        while (node->next != NULL) {
-            node = node->next;
-            index++;
-        }
-        
-        //Put new item into the list
-        node->next = (Item*) malloc(sizeof (Item));
-        node->next->id = index;
-        node->next->str = userInputTemp;
-        node->next->next = 0;
-    }
+
+	char *userInputTemp = (char*) malloc(string_length(userInput) * sizeof (char));
+
+	int i = 0;
+	while (userInput[i]) {//copy string into a new string
+		userInputTemp[i] = userInput[i];
+		i++;
+	}
+	userInputTemp[i] = '\0';
+
+	Item *node = list->root;
+	int id = 0;
+
+	if (node == NULL) {//Insert item into the list for the first time
+		list->root = (Item*) malloc(sizeof (Item));
+		list->root->id = id;
+		list->root->str = userInputTemp;
+		list->root->next = NULL;
+	} else {
+		id++;
+		//Traverse the list until node->next is null
+		while (node->next != NULL) {
+			node = node->next;
+			id++;
+		}
+
+		//Put new item into the non-empty list
+		node->next = (Item*) malloc(sizeof (Item));
+		node->next->id = id;
+		node->next->str = userInputTemp;
+		node->next->next = NULL;
+	}
 }
 
 char* get_history(List* list, int id) {
-    
-    Item *node = list->root;
-    
-    while (node != NULL) {
-        if (node->id == id) {
-            return node->str;
-        }
-        node = node->next;
-    }
+
+	Item *node = list->root;
+
+	while (node != NULL) {
+		if (node->id == id) {//check for the id
+			return node->str; //return the char pointer if matches id
+		}
+		node = node->next; //traverse the list
+	}
 }
 
 void print_history(List* list) {
-    
-    Item *node = list->root;
-    
-    while (node != NULL) {
-        printf("%d - %s\n", node->id, node->str);
-        node = node->next; //assign the next one
-    }
-    printf("------------------------------\n");
+
+	Item *node = list->root;
+
+	while (node != NULL) {//if node isn't null
+		printf("%d - %s\n", node->id, node->str); //print information
+		node = node->next; //traverse the list
+	}
+	printf("------------------------------\n");
 }
 
 void free_history(List* list) {
--- tokenizer.c
+++ tokenizer.c
@@ -6,104 +6,104 @@
 #define ORIGINALSIZE 10
 
 int string_length(char *userInput) {
-    int length = 0; //length holder
-    
-    while (userInput[length] != '\0') {
-        length++; //count each letter
-    }
-    return length;
+	int length = 0; //length holder
+
+	while (userInput[length] != '\0') {
+		length++; //count each letter
+	}
+	return length;
 }
 
 int find_word_start(char* userInput, int index) {
-    while (userInput[index] != '\0') {
-        if (is_valid_character(userInput[index])) {
-            return index; //return index of a valid character
-        }
-        index++; //traversal
-    }
-    return 0;
+	while (userInput[index] != '\0') {
+		if (is_valid_character(userInput[index])) {
+			return index; //return index of a valid character
+		}
+		index++; //traversal
+	}
+	return 0;
 }
 
 int find_word_end(char* userInput, int index) {
-    while (userInput[index] != '\0') {
-        if (!is_valid_character(userInput[index])) {
-            return index; //return index of a invalid character
-        }
-        if (userInput[index + 1] == '\0') {
-            index++;
-            return index; //if NULL character is found
-        }
-        index++;
-    }
+	while (userInput[index] != '\0') {
+		if (!is_valid_character(userInput[index])) {
+			return index; //return index of a invalid character
+		}
+		if (userInput[index + 1] == '\0') {
+			index++;
+			return index; //if NULL character is found
+		}
+		index++;
+	}
 }
 
 char is_valid_character(char c) {
-    return c > 32 && c < 127; //character validation
+	return c > 32 && c < 127; //character validation
 }
 
 int count_words(char* userInput) {
-    printf("Word at: ");
-    int start = 0, end = 0, counted = 0, i = 0;
-    int length = string_length(userInput); //return length of user input
-    
-    while (!(length == i) && !(length - 1 == i)) {
-        start = find_word_start(userInput, end); //keep track of where a word starts
-        end = find_word_end(userInput, start); //keep track of where a word ends
-        printf("[%d, %d] ", start, end);
-        i = end;
-        if (end == 0 && start == 0) break;
-        counted++;
-    }
-    
-    printf("\n");
-    return counted;
+	printf("Word at: ");
+	int start = 0, end = 0, counted = 0, i = 0;
+	int length = string_length(userInput); //return length of user input
+
+	while (!(length == i) && !(length - 1 == i)) {
+		start = find_word_start(userInput, end); //keep track of where a word starts
+		end = find_word_end(userInput, start); //keep track of where a word ends
+		printf("[%d, %d] ", start, end);
+		i = end;
+		if (end == 0 && start == 0) break;
+		counted++;
+	}
+
+	printf("\n");
+	return counted;
 }
 
 char** tokenize(char* userInput) {
-    
-    printf("###Tokenizing###\n");
-    int numWords = count_words(userInput); //amount of words
-    int start = 0, end = 0, i = 0, j = 0, wordCount = 0;
-    char **tokenArr = (char**) malloc((numWords + 1) * sizeof (char*));
-    
-    for (i = 0; i < numWords; i++) {
-        
-        start = find_word_start(userInput, end); //keep track of where a word starts
-        end = find_word_end(userInput, start); //keep track of where a word ends
-        wordCount = end - start;
-        tokenArr[i] = (char*) malloc((wordCount + 1) * sizeof (char)); //allocate memory inside the char**
-        
-        for (j = 0; j < wordCount; j++) {//loop for the size of the word
-            if (userInput[j + start] != ' ') {//while there is no space continue
-                tokenArr[i][j] = userInput[j + start]; //tokenize user input
-            }
-        }
-        tokenArr[i][j] = '\0'; //assign null character
-    }
-    tokenArr[i] = '\0'; //assign null character
-    return tokenArr;
+
+	printf("###Tokenizing###\n");
+	int numWords = count_words(userInput); //amount of words
+	int start = 0, end = 0, i = 0, j = 0, wordCount = 0;
+	char **tokenArr = (char**) malloc((numWords + 1) * sizeof (char*));
+
+	for (i = 0; i < numWords; i++) {
+
+		start = find_word_start(userInput, end); //keep track of where a word starts
+		end = find_word_end(userInput, start); //keep track of where a word ends
+		wordCount = end - start;
+		tokenArr[i] = (char*) malloc((wordCount + 1) * sizeof (char)); //allocate memory inside the char**
+
+		for (j = 0; j < wordCount; j++) {//loop for the size of the word
+			if (userInput[j + start] != ' ') {//while there is no space continue
+				tokenArr[i][j] = userInput[j + start]; //tokenize user input
+			}
+		}
+		tokenArr[i][j] = '\0'; //assign null character
+	}
+	tokenArr[i] = '\0'; //assign null character
+	return tokenArr;
 }
 
 void print_tokens(char** userInput) {
-    
-    printf("###Printing Tokens###\n");
-    int i = 0;
-    
-    while (userInput[i]) {//while there something continue
-        //traverse through tokens
-        printf("Token at %d: %s\n", i, userInput[i]);
-        i++; //increment
-    }
+
+	printf("###Printing Tokens###\n");
+	int i = 0;
+
+	while (userInput[i]) {//while there something continue
+		//traverse through tokens
+		printf("Token at %d: %s\n", i, userInput[i]);
+		i++; //increment
+	}
 }
 
 void free_tokens(char** userInput) {
-    
-    int i = 0;
-    
-    while (userInput[i]) {//while there something continue
-        //free first set of tokens
-        free(userInput[i]);
-        i++; //increment
-    }
-    free(userInput); //free the pointer pointer
+
+	int i = 0;
+
+	while (userInput[i]) {//while there something continue
+		//free first set of tokens
+		free(userInput[i]);
+		i++; //increment
+	}
+	free(userInput); //free the pointer pointer
 }
